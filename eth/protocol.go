a9bd8df90f10433192ad2bdac22f5f08947e2c37
MakeMoneyOz <33920888+MakeMoneyOz@users.noreply.github.com>
2018-02-07 00:23:30 +1100

    Change default networkId

--- eth/protocol.go
+++ eth/protocol.go
@@ -43,7 +43,10 @@ var ProtocolVersions = []uint{eth63, eth62}
 // Number of implemented message corresponding to different protocol versions.
 var ProtocolLengths = []uint64{17, 8}
 
-const ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
+const (
+	NetworkId          = 64
+	ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
+)
 
 // eth protocol message codes
 const (
